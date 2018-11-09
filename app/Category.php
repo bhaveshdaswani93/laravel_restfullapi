5ae716e6bff06c5bf763de92fa0eb157d95883cb
bhavesh <bhaveshdaswani93@gmail.com>
2018-11-09 10:21:00 +0000

    Added get transactions,products,buyers,sellers for the category

--- app/Category.php
+++ app/Category.php
@@ -22,6 +22,10 @@ class Category extends Model
     	'description'
     ];
 
+    protected $hidden = [
+        'pivot'
+    ];
+
     public function products()
     {
     	return $this->belongsToMany(Product::class);
--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,10 @@ Route::resource('sellers', 'Seller\SellerController',['only'=>['index','show']])
  * Category
  */
 Route::resource('categories', 'Category\CategoryController',['except'=>['create','edit']]);
+Route::resource('categories.products','Category\CategoryProductController',['only'=>['index']]);
+Route::resource('categories.sellers','Category\CategorySellerController',['only'=>['index']]);
+Route::resource('categories.transactions','Category\CategoryTransactionController',['only'=>['index']]);
+Route::resource('categories.buyers','Category\CategoryBuyerController',['only'=>['index']]);
 
 /**
  * Product
